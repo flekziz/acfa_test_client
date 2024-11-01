ad680bf27a63169cc1a93ed5cd56b7c4606b8986
usersilva <silvianelson2003@gmail.com>
2024-11-01 15:30:09 +0300

    Добавить background worker

---
+++ src/app/module/Services/BackgroundTasks/GrpcStreamWorker.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace src.app.module.Services.BackgroungTasks
+{
+    public class GrpcStreamWorker : BackgroundService
+    {
+        //private readonly GrpcClient _grpcClient; //grpc клиент 
+
+        //public GrpcStreamWorker(GrpcClient _grpcClient)
+        //{
+        //    _grpcClient = grpcClient;
+        //}
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //throw new System.NotImplementedException();
+            return Task.CompletedTask;
+        }
+    }
+}
+
--- src/app/module/program.cs
+++ src/app/module/program.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using src.app.module.Repositories;
-using AutoMapper;
 using src.app.module.Mapping;
+using src.app.module.Services.BackgroungTasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IConfigurationRepository, InMemoryConfigurationRepository>();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+//регистрация grpc клиента
+//..//
+
+builder.Services.AddHostedService<GrpcStreamWorker>();
 var app = builder.Build();
 
 app.MapControllers();
