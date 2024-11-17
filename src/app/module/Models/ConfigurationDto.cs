b382fd7ea0c4b3b6f77b5d2cbbf0ca468f23a664
usersilva <silvianelson2003@gmail.com>
2024-11-17 21:31:38 +0300

    Добавить dto для взаимодействия с клиентом

---
+++ src/app/module/Models/ConfigurationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace app.module.Models
+{
+    public class ConfigurationDto
+    {
+        internal string Uid { get; set; }
+        internal string DisplayName { get; set; }
+        internal string InternalType { get; set; }
+        internal IEnumerable<PropertyDto> Properties { get; set; }
+        internal IEnumerable<ConfigurationDto> Configurations { get; set; }
+    }
+}
---
+++ src/app/module/Models/PropertyDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace app.module.Models
+{
+    public class PropertDto
+    {
+        internal string Id { get; set; }
+        internal string? ValueString { get; set; }
+        internal IEnumerable<PropertDto>? Properties { get; set; }
+    }
+}
