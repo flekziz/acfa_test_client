a3660d81d48dc68744112702090f5fc138d7ee74
usersilva <silvianelson2003@gmail.com>
2024-11-19 18:35:26 +0300

    Добавить миграции

---
+++ src/repository/module/Migrations/20241119141325_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Configurations",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "text", nullable: false),
+                    DisplayName = table.Column<string>(type: "text", nullable: false),
+                    InternalType = table.Column<string>(type: "text", nullable: false),
+                    ParentUid = table.Column<string>(type: "text", nullable: true),
+                    Properties = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Configurations", x => x.Uid);
+                    table.ForeignKey(
+                        name: "FK_Configurations_Configurations_ParentUid",
+                        column: x => x.ParentUid,
+                        principalTable: "Configurations",
+                        principalColumn: "Uid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "text", nullable: false),
+                    DisplayName = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<int>(type: "integer", nullable: false),
+                    LocalizedString = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Uid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventsData",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    EventId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventsData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventsData_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Uid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Configurations_ParentUid",
+                table: "Configurations",
+                column: "ParentUid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventsData_EventId",
+                table: "EventsData",
+                column: "EventId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Configurations");
+
+            migrationBuilder.DropTable(
+                name: "EventsData");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
